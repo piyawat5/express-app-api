0791b6b12962eda9f7319ac8460b742fe3391030
Piyawat10070400 <piyawat5930302600@gmail.com>
2025-11-02 11:12:45 +0700

    update code

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -22,28 +22,34 @@ import {
   updateConfig,
 } from "../controllers/config.controller.js";
 
+import { login, authen } from "../controllers/authCookie.controller.js";
+import verifyToken from "../config/verify.js";
+
 const router = express.Router();
 
 // ------------- auth --------------
 router.post("/auth/register", validate(registerSchema), register);
-router.get("/approve", getApproveLists);
-router.get("/approve/:id", getApproveListById);
-router.post("/approve/create", createApproveList);
-router.put("/approve/update/:id", updateApproveList);
-router.delete("/approve/delete/:id", deleteApproveList);
-router.get("/user/:userId", getApproveListsByUserId);
+router.get("/approve", verifyToken, getApproveLists);
+router.get("/approve/:id", verifyToken, getApproveListById);
+router.post("/approve/create", verifyToken, createApproveList);
+router.put("/approve/update/:id", verifyToken, updateApproveList);
+router.delete("/approve/delete/:id", verifyToken, deleteApproveList);
+router.get("/user/:userId", verifyToken, getApproveListsByUserId);
 
 router.post("/approve/statusApprove", createStatusApprove);
 router.post("/approve/cronjob/daily-notify", cronjobNotifyPendingApprove);
 
 // ------------- config --------------
-router.get("/config/type", getConfigTypes); //
-router.post("/config/create", createConfig); //
-router.put("/config/update/:id", updateConfig); //
-router.delete("/config/delete/:id", deleteConfig); //
-router.get("/config/:id", getConfigById); //
-router.get("/config", getConfigs); //
-router.post("/config/type/create", createConfigsType); //
+router.get("/config/type", verifyToken, getConfigTypes); //
+router.post("/config/create", verifyToken, createConfig); //
+router.put("/config/update/:id", verifyToken, updateConfig); //
+router.delete("/config/delete/:id", verifyToken, deleteConfig); //
+router.get("/config/:id", verifyToken, getConfigById); //
+router.get("/config", verifyToken, getConfigs); //
+router.post("/config/type/create", verifyToken, createConfigsType); //
+
+router.post("/auth/login", login);
+router.post("/auth/verify", authen);
 
 // TODO: validate
 
